8f0f879ef4ee5c982c5b8df5929a9dba72c0e2b5
Raisul999 <raisulislamarnob0@gmail.com>
2022-12-29 11:05:05 +0600

    form and nav ui fix

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,19 +3,46 @@ import { Grid, TextField, MenuItem } from '@mui/material'
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const Todo = () => {
 
         const [date, setDate] = useState(new Date())
+        const [title, setTitle] = useState('')
+        const [description, setDescription] = useState('')
+        const [priority, setPriority] = useState('')
+
+        console.log(title, date, description, priority)
+
+        // if (title === '') {
+        //         toast.error("Please enter a title")
+        //         return
+
+        // }else if(description==='') {
+        //         toast.error("Please enter a description")
+        //         return
+        // }else if(priority==='') {
+        //         toast.error("Please select a priority")
+        //         return
+
+        // }else if(date==='') {
+        //         toast.error("Please select a date")
+        //         return
+
+        // }
+
         return (
                 <div>
-                        <Grid container spacing={3} justifyContent='center' alignItems='center'>
+                        <Grid container spacing={3} justifyContent='center' alignItems='space-around' flexDirection='column'>
                                 <Grid item md={12}>
                                         <TextField
                                                 id="outlined-required"
                                                 label="Task Title"
                                                 fullWidth
                                                 variant='outlined'
+                                                value={title}
+                                                onChange={(e) => { setTitle(e.target.value) }}
 
                                         />
                                 </Grid>
@@ -27,28 +54,31 @@ const Todo = () => {
                                                 variant='outlined'
                                                 multiline
                                                 maxRows={10}
+                                                value={description}
+                                                onChange={(e) => { setDescription(e.target.value) }}
 
                                         />
                                 </Grid>
                                 <Grid item md={12}>
                                         <TextField
-                                                id="outlined-select-currency"
-                                                select
-                                                label="Priority"
+                                                id="outlined-required"
+                                                label="Task Priority"
                                                 fullWidth
+                                                select
+                                                variant='outlined'
+                                                value={priority}
+                                                onChange={(e) => { setPriority(e.target.value) }}
 
                                         >
-
                                                 <MenuItem value='High'>High</MenuItem>
                                                 <MenuItem value='Medium'>Medium</MenuItem>
                                                 <MenuItem value='Low'>Low</MenuItem>
-
                                         </TextField>
                                 </Grid>
                                 <Grid item md={12}>
                                         <LocalizationProvider dateAdapter={AdapterMoment}>
                                                 <DesktopDatePicker
-                                                        label="Date desktop"
+                                                        label="Deadline"
                                                         inputFormat="MM/DD/YYYY"
                                                         value={date}
                                                         onChange={(newValue) => setDate(newValue)}
