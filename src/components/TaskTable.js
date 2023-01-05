6a8fd7e67d59c8e68de80ce1102547e85ee88683
Raisul999 <raisulislamarnob0@gmail.com>
2023-01-05 17:07:19 +0600

    todo stats

--- src/components/TaskTable.js
+++ src/components/TaskTable.js
@@ -9,28 +9,28 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 
 const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  { id: 'title', label: 'Title', minWidth: 170 },
+  { id: 'description', label: 'Description', minWidth: 100 },
   {
-    id: 'population',
-    label: 'Population',
+    id: 'priority',
+    label: 'Priority',
     minWidth: 170,
     align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
+   
   },
   {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
+    id: 'deadline',
+    label: 'Deadline',
     minWidth: 170,
     align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
+   
   },
   {
-    id: 'density',
-    label: 'Density',
+    id: 'complete',
+    label: 'Complete',
     minWidth: 170,
     align: 'right',
-    format: (value) => value.toFixed(2),
+   
   },
 ];
 
